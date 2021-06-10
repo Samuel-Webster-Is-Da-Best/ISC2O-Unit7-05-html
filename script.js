6027f369d1fa6bf273df86ab5d415e7ef9aed41f
SamuelWebsterIs <84517284+Samuel-Webster-Is-Da-Best@users.noreply.github.com>
2021-06-10 13:53:50 +0000

    stuff

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("button").addEventListener("click", what)
+document.getElementById('button').addEventListener('click', what)
 
 function what () {
-  const day = document.getElementById("day").value
-  const age = document.getElementById("numb").value
+  const day = document.getElementById('day').value
+  const age = document.getElementById('numb').value
   if (day !== 'saturday' && day !== 'sunday') {
     if (age < 18) {
       document.getElementById('answer').innerHTML = 'Time for school!  '
